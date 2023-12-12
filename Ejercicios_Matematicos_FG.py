57e856ef662691df004c640b41e24d3d6fb29637
Luis170804 <153634517+Luis170804@users.noreply.github.com>
2023-12-12 09:54:54 -0500

    Add files via upload

---
+++ Ejercicios_Matematicos_FG.py
@@ -0,0 +1,55 @@
+#Este programa nos ayuda a resolver los ejercicios matermaticos de manera mas rapida y eficiente ya que nos proporciona un resultado exacto sin fallos.
+
+#EJERCICIO 1
+
+#Suma de dos numeros int
+a=822
+b=193
+c=(a+b)
+print(c)
+
+#Resta de dos numeros de punto float
+a=19.1
+b=82.1
+d=(a-b)
+print(d)
+
+#Muliplicacion de numeros int
+a=82
+b=20
+e=(a*b)
+print(e)
+
+#Divicion de dos numeros de float
+a=82.2
+b=10.2
+z=(a/b)
+print(z)
+
+#EJERCICIO 2
+
+Nombre_completo = "Luis_Fernando_González_Fabara"
+len(Nombre_completo)
+print(len(Nombre_completo))
+print((Nombre_completo),len(Nombre_completo))
+#Este programa es muy importante en porque nos ayuda a saber la longitud de nuestro nombre completo.
+
+#EJERCICIO 3
+
+# Paso 1: Declara una variable entera y otra de punto flotante
+i = 17
+pf = 9.21
+#Cambio_de_variable_tipo_int_a_float
+print(i)
+print(float(i))
+#Cambio_de_variable_tipo_float_a_int
+print(pf)
+print(int(pf))
+
+#EJERCICIO_4
+#Ejercicio_1:En el ejercicio numero 1 se realizo una programación la cual nos ayude a sumar. restar, multiplicar y división, esta nos ayudara a tener un resultado preciso.
+#Ejercicio_2:En el eercicio numero 2 realizace una programación con la variable strin para saber la longitud exacta de nuestro nombre completo.
+#Ejercicio_3:En el ejercicio numero 3 realice una programación en la cual nos ayudara a cambiar una variable de punto entero a un punto flotante y viceversa.
+
+
+
